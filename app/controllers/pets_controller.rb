761a0395ff8f506e393f90583eb079ed09f4dd42
ellenupark <ellenupark@gmail.com>
2020-11-03 23:35:33 -0500

    Added default image for new pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,6 +16,11 @@ class PetsController < ApplicationController
   def create
     @pet = Pet.new(pet_params)
     @pet.user = current_user
+
+    if !@pet.avatar.attached?
+      @pet.avatar.attach(io: File.open('./public/default-profile.jpg'), filename: 'default-profile.jpg')
+    end
+
     if @pet.save
       redirect_to pet_path(@pet)
     else
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,6 @@ class Pet < ApplicationRecord
     has_many :events, through: :invites
     belongs_to :user
     has_one_attached :avatar
+
+    validates :name, :breed, :weight, :gender, presence: true
 end
