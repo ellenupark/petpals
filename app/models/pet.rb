841e13cfc3ca1dda8e0324b614d7cb8174fcdf6e
ellenupark <ellenupark@gmail.com>
2020-11-10 20:12:02 -0500

    Added age method to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,11 +6,14 @@ class Pet < ApplicationRecord
 
     validates :name, :breed, :weight, :gender, :birthdate, presence: true
 
-    def age_in_years
-        ((Time.zone.now - self.birthdate.to_time) / 1.year.seconds).floor
-    end
-
-    def age_in_months
-        (Time.now.utc.to_date.year * 12 + Time.now.utc.to_date.month) - (self.birthdate.year * 12 + self.birthdate.month)
+    def age
+        if ((Time.zone.now - self.birthdate.to_time) / 1.year.seconds).floor == 0
+            age = (Time.now.utc.to_date.year * 12 + Time.now.utc.to_date.month) - (self.birthdate.year * 12 + self.birthdate.month)
+            age = "#{age} #{"month".pluralize(age)} old"
+        else
+            age = ((Time.zone.now - self.birthdate.to_time) / 1.year.seconds).floor
+            age = "#{age} #{"year".pluralize(age)} old"
+        end
+        age
     end
 end
