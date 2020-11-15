a63622ebc8b0f0531e5fb481dfa3729bfed9a2c8
ellenupark <ellenupark@gmail.com>
2020-11-14 22:14:33 -0500

    Added error messages to event decline and cancel

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -49,13 +49,17 @@ class EventsController < ApplicationController
         @events = current_user.unconfirmed_host_events
     end
 
-    def decline
-        redirect_to event_path(params[:id])
-    end
+    # def decline
+    #     redirect_to event_path(params[:id])
+    # end
 
     def destroy
+        if @event.accepted == false
+            @event.host == current_user ? flash[:notice] = "Successfully cancelled invitation." : flash[:notice] = "Successfully declined playdate."
+        else
+            flash[:notice] = "Successfully cancelled playdate."
+        end
         @event.destroy
-        #add cancel notification?
         redirect_to user_path(current_user.id)
     end
 
